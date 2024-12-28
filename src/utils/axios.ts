f24f4fd0a0c1eb7ee37268feaba7151ab3192ad3
Camille HACQUET <camille@hacquet.com>
2024-12-28 13:58:22 +0100

    🎨 Improved code structure

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,8 +2,8 @@ import { randomUUID } from 'node:crypto';
 import { Injectable } from '@nestjs/common';
 import axios, {
 	AxiosError,
-	type AxiosResponse,
 	type AxiosInstance,
+	type AxiosResponse,
 	type CreateAxiosDefaults,
 } from 'axios';
 import type { ICustomAxiosRequestConfig } from 'src/interfaces/axios.interface';
@@ -19,57 +19,47 @@ const {
 @Injectable()
 export class Axios {
 	static create(config?: CreateAxiosDefaults): AxiosInstance {
-		const axiosInstance = axios.create(config);
+		const instance = axios.create(config);
 
-		axiosInstance.interceptors.request.use(
-			Axios.handleRequest,
-			Promise.reject,
-		);
-
-		axiosInstance.interceptors.response.use(
+		instance.interceptors.request.use(Axios.handleRequest, Promise.reject);
+		instance.interceptors.response.use(
 			Axios.handleResponse,
 			Axios.handleResponse,
 		);
 
-		return axiosInstance;
+		return instance;
 	}
 
 	private static handleRequest(
-		request: ICustomAxiosRequestConfig,
+		config: ICustomAxiosRequestConfig,
 	): ICustomAxiosRequestConfig {
-		request.id = randomUUID();
-		request.startTime = Date.now();
-		request.headers['User-Agent'] ||= `${APP_TITLE}/${APP_VERSION}`;
+		config.id = randomUUID();
+		config.startTime = Date.now();
+		config.headers['User-Agent'] ||= `${APP_TITLE}/${APP_VERSION}`;
 
 		if (REQUEST_HTTP_PROXY_ENABLED === 'true')
-			request.proxy = {
+			config.proxy = {
 				host: REQUEST_HTTP_PROXY_HOST,
 				port: Number(REQUEST_HTTP_PROXY_PORT),
 			};
 
-		return request;
+		return config;
 	}
 
-	private static handleResponse(
-		result: AxiosResponse<unknown, ICustomAxiosRequestConfig> | AxiosError,
-	) {
-		const isError = result instanceof AxiosError;
+	private static handleResponse(responseOrError: AxiosResponse | AxiosError) {
+		const isError = responseOrError instanceof AxiosError;
+		const config = responseOrError.config as ICustomAxiosRequestConfig;
 
-		const config = result.config as ICustomAxiosRequestConfig;
-		const headers = isError
-			? (result as AxiosError)?.response?.headers
-			: (result as AxiosResponse)?.headers;
+		if (config?.startTime) {
+			const headers = isError
+				? responseOrError.response?.headers
+				: responseOrError.headers;
+			if (headers)
+				headers['Request-Duration'] = String(
+					Date.now() - config.startTime,
+				);
+		}
 
-		if (headers) Axios.addRequestDuration(config, headers);
-
-		return isError ? Promise.reject(result) : result;
-	}
-
-	private static addRequestDuration(
-		config?: ICustomAxiosRequestConfig,
-		headers?: Record<string, unknown>,
-	): void {
-		if (config?.startTime && headers)
-			headers['Request-Duration'] = String(Date.now() - config.startTime);
+		return isError ? Promise.reject(responseOrError) : responseOrError;
 	}
 }
